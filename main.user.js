7d972501709ea764b76b5b70ab70caeb5da3999e
L0laapk3 <L0laapk3@gmail.com>
2017-10-20 20:59:46 +0200

    fix

--- main.user.js
+++ main.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         fuck facebook event linker
-// @version      1
+// @version      1.1
 // @description  facebook sucks ass
 // @author       You
 // @match        https://www.messenger.com/*
 // @grant        none
 // ==/UserScript==
 
-window.document.styleSheets[0].insertRule("div[message] a[href='#'] { cursor: text !important; text-decoration: none !important; }", sheet.cssRules.length);
+var sheet = document.createElement('style');
+sheet.innerHTML = "div[message] a[href='#'] { cursor: text !important; text-decoration: none !important; }";
+document.head.appendChild(sheet);
