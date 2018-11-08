da17903a8cbf37c8f796c2953cae85e4f477bd16
L0laapk3 <L0laapk3@gmail.com>
2018-11-08 19:59:30 +0100

    updated

--- main.user.js
+++ main.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         fuck facebook event linker
-// @version      1.1
+// @version      2.0
 // @description  facebook sucks ass
+// @downloadURL  https://cdn.jsdelivr.net/gh/L0laapk3/fuck-facebook-event-linker/main.user.js
 // @author       You
 // @match        https://www.messenger.com/*
 // @grant        none
 // ==/UserScript==
 
 var sheet = document.createElement('style');
-sheet.innerHTML = "div[message] a[href='#'] { cursor: text !important; text-decoration: none !important; }";
+sheet.innerHTML = "a[href='#'] { cursor: text !important; text-decoration: none !important; }";
 document.head.appendChild(sheet);
